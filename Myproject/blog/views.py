f82953e83497b9685f0a79a944ebcbb4d6ed3424
Mahdi Nazari <mahdinazari830@gmail.com>
2021-06-27 22:22:48 +0430

    Make custom template tags(simple, inclusion), make view for the categories

--- Myproject/blog/views.py
+++ Myproject/blog/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+#from django.http import HttpResponse, JsonResponse, Http404
 from .models import Article, Category
 
 # Create your views here.
 def home(request):
     context = {
-        "articles": Article.objects.filter(status = "p"),
-        "category": Category.objects.filter(status = True)
+        "articles": Article.objects.filter(status = "p")
     }
     return render(request, "blog/home.html", context)
 
 def detail(request, slug):
     context = {
-        "article": get_object_or_404(Article, slug = slug, status = "p"),
-        "category": Category.objects.filter(status = True)
+        "article": get_object_or_404(Article, slug = slug, status = "p")
     }
     return render(request, "blog/detail.html", context)
+
+def category(request, slug):
+    context = {
+        "category": get_object_or_404(Category, slug = slug, status = True)
+    }
+    return render(request, "blog/category.html", context)
