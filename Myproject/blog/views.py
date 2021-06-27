959edafbf5577ea7185f3fd74660df9a6e7a0f5f
Mahdi Nazari <mahdinazari830@gmail.com>
2021-06-27 17:03:50 +0430

    Show categories in navbar menu

--- Myproject/blog/views.py
+++ Myproject/blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Article
+from .models import Article, Category
 
 # Create your views here.
 def home(request):
     context = {
-        "articles": Article.objects.filter(status = "p").order_by('-publish')[:3]
+        "articles": Article.objects.filter(status = "p"),
+        "category": Category.objects.filter(status = True)
     }
     return render(request, "blog/home.html", context)
 
 def detail(request, slug):
     context = {
-        "article": get_object_or_404(Article, slug = slug, status = "p")
+        "article": get_object_or_404(Article, slug = slug, status = "p"),
+        "category": Category.objects.filter(status = True)
     }
     return render(request, "blog/detail.html", context)
